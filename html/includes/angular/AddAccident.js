30f09c0a80fff60f2e451e18c1f2ce746a0cc62b
Dane Summers <dsummersl@yahoo.com>
2015-03-10 10:41:35 -0400

    Improves 'add accident' layout, stubs form submission.

--- html/includes/angular/AddAccident.js
+++ html/includes/angular/AddAccident.js
@@ -1,4 +1,7 @@
 OCEM.controller('addAccidentController', ['$scope','$firebase','datasetSettings',
 function ($scope, $firebase, datasetSettings) {
   $scope.questions = datasetSettings;
+  $scope.addAccident = function() {
+    console.log("|add!");
+  };
 }]);
